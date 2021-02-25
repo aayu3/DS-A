defed2415dbe5c89b28aba33656af520f4686aae
Aaron Yu <aaronyu3@illinois.edu>
2021-02-25 14:15:11 -0800

    Finished frame_print function

--- assignment_0.cpp
+++ assignment_0.cpp
@@ -24,11 +24,29 @@ std::string inverse(std::string input) {
 
 void frame_print(std::string arr[]) {
     int max = -1;
-    
+    int len = *(&arr + 1) - arr - 1;
+    // I believe this -1 is because you are passing a pointer to arr in the function.
+    for (int i = 0; i < len; i++) {
+        int strLen = arr[i].length();
+        // I don't quite understand why, but I need to cast arr[i].length() to an int;
+        if (strLen > max) {
+            max = strLen;
+        }
+    }
+    std::string horizontalBoarder(max+2,'*');
+    std::cout << horizontalBoarder << "\n";
+    for (int i = 0; i < len; i++) {
+        std::cout << "*" + arr[i];
+        std::string spaces(max-arr[i].length(), ' ');
+        std::cout << spaces + "*" << "\n";
+    }
+    std::cout << horizontalBoarder << "\n";
 }
 
 int main() {
     std::cout << "Hello Word" << "\n";
     std::string result =  inverse("Nice Dog!");
     std::cout << result << "\n";
+    std::string arr[6] = {"Hello", "Word", "In", "A", "Frame", "Yeet"};
+    frame_print(arr);
 }
