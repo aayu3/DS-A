3934d31ff783cdb6f01343020eb8155fabdca993
Aaron Yu <aaronyu3@illinois.edu>
2021-02-24 16:28:37 -0800

    Added more problems to assignment 0

--- assignment_0.cpp
+++ assignment_0.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 class Proofnik {
     public:
         std::string name;
@@ -11,6 +12,23 @@ class Proofnik {
 
 }; 
 
-int main() {
+std::string inverse(std::string input) {
+    int len = input.length();
+    std::string reverse = "";
+    for (int i = len - 1; i >= 0; i--) {
+        reverse = reverse + input[i];
+    }
+    return reverse;
+
+}
+
+void frame_print(std::string arr[]) {
+    int max = -1;
     
+}
+
+int main() {
+    std::cout << "Hello Word" << "\n";
+    std::string result =  inverse("Nice Dog!");
+    std::cout << result << "\n";
 }
